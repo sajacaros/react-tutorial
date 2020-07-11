60f0c513034df536ee3aa77c67a1910c368609a9
dukim <sajacall82@naver.com>
2020-07-11 21:30:38 +0900

    src path 수정

--- src/ch01/RCC.jsx
+++ src/ch01/RCC.jsx
@@ -1,16 +1,10 @@
 // rcc
 import React, { Component } from 'react';
 
-
-
 class RCC extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return <div></div>;
+  }
 }
 
 export default RCC;
