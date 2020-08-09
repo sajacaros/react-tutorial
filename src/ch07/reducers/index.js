da2eea342d5f16c388877d52bc2e885084eff7ce
dukim <sajacall82@naver.com>
2020-08-09 12:08:38 +0900

    수정

--- src/ch07/reducers/index.js
+++ src/ch07/reducers/index.js
@@ -2,6 +2,7 @@ import loading from './loadingReducer';
 import user from './userReducer';
 import collection from './collectionReducer02';
 import searchFilter from './searchFilterReducer';
+
 export default {
   loading,
   user,
